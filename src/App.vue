bcf9b50090bc5bd6846444a5649a9f08b9fad68a
vitalii-minchuk <mcmin4doutsch@gmail.com>
2023-02-14 09:01:00 +0200

    icons and svgs

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GitHubSVG from './assets/icons/GitHubSVG.vue'
+import WhatTime from './components/WhatTime/WhatTime.vue'
+</script>
 
 <template>
-  <header></header>
+  <v-app>
+    <v-app-bar class="px-6">
+      <v-alert-title>What time</v-alert-title>
+      <v-spacer></v-spacer>
+      <v-row justify="end">
+        <v-btn>
+          <a
+            href="https://github.com/vitalii-minchuk/what-time"
+            target="_blank"
+          >
+            <GitHubSVG />
+          </a>
+        </v-btn>
+        <v-btn> ok </v-btn>
+      </v-row>
+    </v-app-bar>
 
-  <main>
-    <v-btn>ok</v-btn>
-  </main>
+    <main class="mt-16">
+      <WhatTime />
+    </main>
+  </v-app>
 </template>
---
+++ src/components/WhatTime/WhatTime.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <v-card>
+      <LineAccountSVG />
+    </v-card>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import LineAccountSVG from '@/assets/icons/LineAccountSVG.vue'
+</script>
+
+<style scoped></style>
