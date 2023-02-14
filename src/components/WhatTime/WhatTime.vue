a822416b4efdd2aeb6423361b4e15e8626aba25b
vitalii-minchuk <mcmin4doutsch@gmail.com>
2023-02-14 11:04:16 +0200

    timezones list

--- src/components/WhatTime/WhatTime.vue
+++ src/components/WhatTime/WhatTime.vue
@@ -1,13 +1,39 @@
 <template>
   <v-container>
-    <v-card>
-      <LineAccountSVG />
-    </v-card>
+    <input v-model="input" type="text" />
+    <div>
+      <div v-for="item in searchResult" :key="item.text">
+        <div @click="setZone(item)">
+          {{ item.text }}
+        </div>
+      </div>
+    </div>
+    <div>
+      <div v-for="zone in zones" :key="zone.name">
+        {{ zone.name }} {{ zone.offset }}
+      </div>
+    </div>
   </v-container>
 </template>
 
 <script setup lang="ts">
-import LineAccountSVG from '@/assets/icons/LineAccountSVG.vue'
+import type { Timezone } from '@/types'
+import timezones from 'timezones.json'
+import { computed, ref } from 'vue'
+
+const zones = ref<Timezone[]>([])
+const input = ref('')
+const searchResult = computed(() =>
+  timezones.filter((t) => t.utc.find((u) => u.includes(input.value)))
+)
+function setZone(item: (typeof timezones)[0]) {
+  const newZone: Timezone = {
+    offset: item.offset,
+    name: item.text
+  }
+
+  zones.value.push(newZone)
+}
 </script>
 
 <style scoped></style>
---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export interface Timezone {
+  name: string
+  offset: number
+}
